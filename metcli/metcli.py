50ee956261322f0a7168306fb36db9a5ca16b57c
Ozzy Walsh <18150509+ozzywalsh@users.noreply.github.com>
2019-01-20 19:41:58 +0000

    Fetch and display national forecast

--- metcli/metcli.py
+++ metcli/metcli.py
@@ -1,2 +1,45 @@
+import textwrap
+import sys
+from termcolor import colored
+import requests
+
+
+def met_request(endpoint):
+    try:
+        url = requests.compat.urljoin('https://www.met.ie/api/', endpoint)
+        return requests.get(url).json()
+    except requests.exceptions.ConnectionError:
+        print('There was a problem connecting to Met Éireann.')
+        sys.exit()
+
+
+def get_national():
+    return met_request('weather/national')
+
+
 def run():
-    print('it works!')
+    panel_names = [
+        'today',
+        'tonight',
+        'tomorrow',
+    ]
+
+    panel_template = '{title} - {date}\n{text}'
+
+    national = get_national()
+    forecast_panels = national['forecastText']['forecastPanels']
+
+    panel_texts = []
+
+    for panel_name in panel_names:
+        panel = forecast_panels.get(panel_name)
+        if panel:
+            title, text, date = panel.values()
+            wrapped_text = '\n'.join(textwrap.wrap(text, width=80))
+            text = panel_template.format(title=colored(title, 'red'),
+                                         date=colored(date, 'green'),
+                                         text=wrapped_text
+                                         )
+
+            panel_texts.append(text.strip())
+    print('\n\n'.join(panel_texts))
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
     description="A cli interface to Met Eireann",
     install_requires=[
         'requests'
+        'termcolor'
     ],
     entry_points={
         'console_scripts': ['metcli=metcli.command_line:main']
