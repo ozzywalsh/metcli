b0a29e1a9a1468c5f85fd6a520579083a54c77fa
Ozzy Walsh <18150509+ozzywalsh@users.noreply.github.com>
2019-01-20 23:17:08 +0000

    Remove double quotes in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name='metcli',
     author='Ozzy Walsh',
-    description="A cli interface to Met Eireann",
+    description='A cli interface to Met Eireann',
     install_requires=[
         'requests',
         'termcolor'
