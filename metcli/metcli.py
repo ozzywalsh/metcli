a926a81719f74b3fa56ae288b1e8098e645735a3
Ozzy Walsh <18150509+ozzywalsh@users.noreply.github.com>
2019-01-20 23:11:01 +0000

    Refactor into smaller functions

--- metcli/metcli.py
+++ metcli/metcli.py
@@ -16,16 +16,25 @@ def get_national():
     return met_request('weather/national')
 
 
-def run():
+def format_panel(panel):
+    panel_template = '{title} - {date}\n{text}'
+    title, text, date = panel.values()
+    wrapped_text = '\n'.join(textwrap.wrap(text, width=80))
+    text = panel_template.format(title=colored(title, 'red'),
+                                 date=colored(date, 'green'),
+                                 text=wrapped_text
+                                 )
+
+    return text.strip()
+
+
+def format_national(national):
     panel_names = [
         'today',
         'tonight',
         'tomorrow',
     ]
 
-    panel_template = '{title} - {date}\n{text}'
-
-    national = get_national()
     forecast_panels = national['forecastText']['forecastPanels']
 
     panel_texts = []
@@ -33,12 +42,11 @@ def run():
     for panel_name in panel_names:
         panel = forecast_panels.get(panel_name)
         if panel:
-            title, text, date = panel.values()
-            wrapped_text = '\n'.join(textwrap.wrap(text, width=80))
-            text = panel_template.format(title=colored(title, 'red'),
-                                         date=colored(date, 'green'),
-                                         text=wrapped_text
-                                         )
+            panel_texts.append(format_panel(panel))
 
-            panel_texts.append(text.strip())
-    print('\n\n'.join(panel_texts))
+    return '\n\n'.join(panel_texts)
+
+
+def run():
+    national = get_national()
+    print(format_national(national))
