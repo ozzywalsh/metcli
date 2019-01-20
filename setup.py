338ded9096a4a0ed0cec81b0f82f8d95565a22b0
Ozzy Walsh <18150509+ozzywalsh@users.noreply.github.com>
2019-01-20 23:46:33 +0000

    Load README in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 import setuptools
 
+# read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setuptools.setup(
     name='metcli',
+    version='0.1.0',
     author='Ozzy Walsh',
-    description='A cli interface to Met Eireann',
+    description='A command line interface for Met Éireann.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
     install_requires=[
         'requests',
         'termcolor'
